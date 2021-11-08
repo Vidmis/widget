66c04ffcf49246c0e3c3f22bb3f1977d24387928
Vidmis <v.gutauskas90@gmail.com>
2021-11-08 16:15:10 +0200

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,34 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addProduct, addPrice, subtractPrice } from "../features/orderSlice";
 import useFetch from "../hooks/useFetch.js";
-<<<<<<< HEAD
-import { CardStyle } from "./styles/Card.style.js";
-import { Form, List } from "./styles/Products.style";
-=======
 import Card from "./styles/CardUi/Card.js";
 import styles from "./Products.module.scss";
->>>>>>> prodBuild
 
 const Products = ({ setStep }) => {
   const dispatch = useDispatch();
   const [isDisabled, setIsDisabled] = useState(false);
-<<<<<<< HEAD
-=======
   const [clickedItems, setClickedItems] = useState([]);
->>>>>>> prodBuild
   const { data: products } = useFetch(
     "https://run.mocky.io/v3/b5eb9a17-4e56-4841-bb9a-094cd3fcec96"
   );
 
-<<<<<<< HEAD
-  const handleSelect = (e) => {
-    if (e.target.checked) {
-      dispatch(addPrice(Number(e.target.value)));
-      setSelectedProd([...selectedProd, e.target.name]);
-    } else if (selectedProd.some((val) => val === e.target.name)) {
-      dispatch(subtractPrice(Number(e.target.value)));
-      setSelectedProd(selectedProd.filter((val) => val !== e.target.name));
-=======
   const handleSelect = (id, price) => {
     if (clickedItems.find((item) => item === id)) {
       dispatch(subtractPrice(Number(price)));
@@ -37,7 +20,6 @@ const Products = ({ setStep }) => {
     } else {
       dispatch(addPrice(Number(price)));
       setClickedItems([...clickedItems, id]);
->>>>>>> prodBuild
     }
   };
 
@@ -62,28 +44,10 @@ const Products = ({ setStep }) => {
           e.preventDefault();
           handleSubmit();
         }}
-<<<<<<< HEAD
-        onChange={(e) => handleSelect(e)}
-=======
->>>>>>> prodBuild
       >
         <h3>Select product(s)</h3>
         <ul>
           {products?.map(({ id, title, price }) => (
-<<<<<<< HEAD
-            <List key={id}>
-              <input
-                type='checkbox'
-                id={id}
-                value={price.amount}
-                name={id}
-                className='checkbox'
-              />
-              <label htmlFor={id}>
-                {title} {price.amount} €
-              </label>
-            </List>
-=======
             <li
               key={id}
               onClick={() => handleSelect(id, price.amount)}
@@ -95,7 +59,6 @@ const Products = ({ setStep }) => {
             >
               {title} - {price.amount} €
             </li>
->>>>>>> prodBuild
           ))}
         </ul>
         <button type='submit' disabled={!isDisabled}>
